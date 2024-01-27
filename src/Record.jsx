810099e28d850401debc706a104ee5cb3a709b35
Homesh Wathsalya Ampe Mohottige <homeshw@Homeshs-MacBook-Pro.local>
2024-01-27 21:35:47 +0530

    save recording to a file

--- src/Record.jsx
+++ src/Record.jsx
@@ -1,11 +1,15 @@
 import { AudioRecorder, useAudioRecorder } from 'react-audio-voice-recorder';
 import React, { useState } from 'react';
 import axios from 'axios';
+import TextField from '@mui/material/TextField'; 
+import Button from '@mui/material/Button';
 
 function RecordPage() {
 
     const [file, setFile] = useState(null);
     const [url, setUrl] = useState(null);
+
+    const [fileName, setFileName] = useState('');
 
     const serverPort = 5001;
 
@@ -25,12 +29,6 @@ function RecordPage() {
         });
         handleFileUpload(audiofile)
         setFile(audiofile)
-
-        // const url = URL.createObjectURL(blob);
-        // const audio = document.createElement("audio");
-        // audio.src = url;
-        // audio.controls = true;
-        // document.body.appendChild(audio);
     };
 
     const handleFileChange = (blob) => {
@@ -46,9 +44,14 @@ function RecordPage() {
     };
 
     const handleFileUpload = async (f) => {
+
+        if(fileName == '' || fileName == null){
+            alert('empty file name');
+            return;
+        }
         try {
             const audioBlob = await fetch(url).then((r) => r.blob());
-            const audioFile = new File([audioBlob], 'voice.wav', { type: 'audio/wav' });
+            const audioFile = new File([audioBlob], fileName + '.wav', { type: 'audio/wav' });
             const formData = new FormData();
             formData.append('file', audioFile);
 
@@ -74,10 +77,11 @@ function RecordPage() {
                 //onRecordingComplete={(blob) => handleFileChange(blob)}
                 recorderControls={recorderControls}
             />
-            <button onClick={recorderControls.stopRecording}>Stop recording</button>
+            <TextField id="label" label="Input Text" variant="outlined" onChange={(e) => setFileName(e.target.value)}/>
+            {/* <button onClick={recorderControls.stopRecording}>Stop recording</button> */}
 
-            <input type="file" onChange={handleFileChange} />
-            <button onClick={handleFileUpload}>Upload File</button>
+            {/* <input type="file" onChange={handleFileChange} /> */}
+            <Button onClick={handleFileUpload}>Save Recording</Button>
         </div>
 
 
