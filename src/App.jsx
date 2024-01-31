31efd56c7e7b423bf68748b5c0334a6014491c8a
Homesh Wathsalya Ampe Mohottige <homeshw@Homeshs-MacBook-Pro.local>
2024-01-31 19:32:47 +0530

    Add navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,23 @@ import React, { useState, useEffect } from 'react';
 import Test1 from './Test1';
 import RecordPage from './Record';
 import MainPage from './Main';
+import NavBar from './NavBar';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/test1" element={<Test1 />} />
-        <Route path="/record" element={<RecordPage />} />
-      </Routes>
-    </Router>
+    <>
+      <NavBar></NavBar>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/test1" element={<Test1 />} />
+          <Route path="/record" element={<RecordPage />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 export default App;
