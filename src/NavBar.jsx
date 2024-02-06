b1848838b81c6e49555700a3cfc2e6eac5523205
Homesh Wathsalya Ampe Mohottige <homeshw@Homeshs-MacBook-Pro.local>
2024-02-06 18:55:43 +0530

    bugfix: navlinks

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -12,9 +12,9 @@ function NavBar(){
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="..">Home</Nav.Link>
-                <Nav.Link href="../Record">Record</Nav.Link>
-                <Nav.Link href="../Test1">Test 1</Nav.Link>
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/Record">Record</Nav.Link>
+                <Nav.Link href="/Test1">Test 1</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
