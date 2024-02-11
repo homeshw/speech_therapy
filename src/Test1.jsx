837ef7946cc5f13e569d806dc4bea137b7df0920
Homesh Wathsalya Ampe Mohottige <homeshw@Homeshs-MacBook-Pro.local>
2024-02-11 09:54:14 +0530

    try toggle

--- src/Test1.jsx
+++ src/Test1.jsx
@@ -5,22 +5,28 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import Switch from '@mui/material/Switch';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
 import axios from 'axios';
 import './App.css';
 
 function Test1() {
-  const [selectedWord, setSelectedWord] = useState(null);
+  //const [selectedWord, setSelectedWord] = useState(null);
   const [currentClip, setCurrentClip] = useState({});
   const [message, setMessage] = useState({});
   const [showPopup, setShowPopup] = useState(false);
-  const [playAudio, setPlayAudio] = useState(false);
+  //const [playAudio, setPlayAudio] = useState(false);
   const [open, setOpen] = React.useState(false);
   const [clipToggle, setClipToggle] = React.useState(false);
-  const handleOpen = () => setOpen(true);
+  //const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const [tryWord, setTryWord] = useState(false);
 
   // Access the API endpoint
   const apiEndpoint = window.config.REACT_APP_API_ENDPOINT;
+
+  //const testToggle = false;
 
   //console.log(window.config.REACT_APP_API_ENDPOINT);
 
@@ -29,6 +35,8 @@ function Test1() {
   const [audioUrl, setAudioUrl] = useState('');
 
   const [testArray, setTestArray] = useState(null);
+
+  const audioRef = useRef(null);
 
   const selectRandomClip = () => {
     const randomIndex = Math.floor(Math.random() * testArray.length);
@@ -58,10 +66,55 @@ function Test1() {
     }
   }, [currentClip])
 
-  const handleWordSelect = word => {
-    setSelectedWord(word);
-    word === currentClip.word ? setMessage('Correct!') : setMessage('Incorrect!')
-    setShowPopup(true);
+  // useEffect(() => {
+  //   if(testToggle) {
+
+      
+  //     setPlayAudio(true)
+  //     console.log(playAudio)
+  //   }
+  //   else {
+  //     setPlayAudio(false)
+  //   }
+
+  // }, [audioUrl])
+
+  const handleWordSelect = async word => {
+
+    if(tryWord) {
+      if(testArray.length > 0) {
+
+        let selObj = testArray.find(item => item.word === word)
+        setAudioUrl(apiEndpoint + '/get/audio/' + selObj.src)
+
+        const audioElement = document.getElementById('audio-player');
+
+      if(audioElement) {
+
+        // console.log(audioElement.currentTime)
+        // console.log(audioElement.paused)
+        // console.log(audioElement.ended)
+        // console.log(audioElement.readyState)
+        // console.log(audioElement.HAVE_CURRENT_DATA)
+        audioElement.pause() // no apperent effect
+        
+        console.log(audioElement.currentTime)
+        audioElement.play().catch(error => {
+          // todo: find a permenent solution
+          audioElement.play();
+          console.error('Failed to play audio: ' + error)
+        });
+      }
+
+      }
+    }
+    else {
+      //setSelectedWord(word);
+      word === currentClip.word ? setMessage('Correct!') : setMessage('Incorrect!')
+      setShowPopup(true);
+    }
+    
+    
   };
 
   const handleClosePopup = () => {
@@ -70,16 +123,16 @@ function Test1() {
     setClipToggle(!clipToggle);
   };
 
-  const fetchAudio = async () => {
-    console.log("fetching audio")
-    try {
-      console.log('current clip obj: ')
-      console.log(currentClip)
-      const response = await axios.get(apiEndpoint + '/get/audio/' + currentClip.src)
-    } catch (error) {
-      console.error('Error fetching audio: ', error);
-    }
-  }
+  // const fetchAudio = async () => {
+  //   console.log("fetching audio")
+  //   try {
+  //     console.log('current clip obj: ')
+  //     console.log(currentClip)
+  //     const response = await axios.get(apiEndpoint + '/get/audio/' + currentClip.src)
+  //   } catch (error) {
+  //     console.error('Error fetching audio: ', error);
+  //   }
+  // }
 
   const fetchArray = async () => {
     try {
@@ -94,7 +147,11 @@ function Test1() {
 
   return (
     <>
-      <div className="audio-player"><AudioPlayer src={audioUrl} /></div>
+    <FormGroup>
+      <FormControlLabel control={<Switch checked={tryWord} onChange={ (e) => setTryWord(e.target.checked)}/>} label="try!" />
+    </FormGroup>
+
+      <div className="audio-player"><AudioPlayer ref={audioRef} src={audioUrl} play={false} /></div>
       
       {testArray && (
         <WordSelector words={testArray} onSelect={handleWordSelect} />
