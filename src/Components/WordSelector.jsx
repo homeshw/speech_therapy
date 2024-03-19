615bed4a11136752e3d69f98adf69040e5febaef
niroshc88 <niroshc88@gmail.com>
2024-03-20 08:29:36 +1100

    fixed: display issue in large number of answers in test page

--- src/Components/WordSelector.jsx
+++ src/Components/WordSelector.jsx
@@ -5,7 +5,7 @@ import * as React from 'react';
 
 const WordSelector = ({ words, onSelect }) => {
   return (
-    <div style={{ position: 'relative', display: 'flex', justifyContent: 'space-evenly' }}>
+    <div style={{ position: 'relative', display: 'flex', justifyContent: 'space-evenly', flexWrap: 'wrap' }}>
       {words && words.map((word) => {
         return (
           <button className="default-button button-choice" onClick={()=> onSelect(word)}>
